4bca00415d2f9e8b18647c69fc7afe279af8b2bd
Taras Ishchuk <ishchuktaras@gmail.com>
2021-12-31 13:55:26 +0200

    commit just copying fucking code because can't got it

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const hoursElement = document.getElementById('hour_hand');
+const textElement = document.getElementById("text");
+const hoursElement = document.getElementById("hour_hand");
+const minutesElement = document.getElementById("minute_hand");
+const secondsElement = document.getElementById("second_hand");
 
-const date = new Date();
-const hour = date.getHours;
+let showDate = true;
 
-hoursElement.setAttribute('transform','rotate(${(360/12)}');
+function animate() {
+  const date = new Date();
+
+  const day = date.getDate();
+  const ampm = date.getHours() >= 12 ? "PM" : "AM";
+  const hour = date.getHours() + date.getMinutes() / 60;
+  const minute = date.getMinutes() + date.getSeconds() / 60;
+  const second = date.getSeconds() + date.getMilliseconds() / 1000;
+
+  textElement.textContent = showDate ? day : ampm;
+  hoursElement.setAttribute("transform", `rotate(${(360 / 12) * hour})`);
+  minutesElement.setAttribute("transform", `rotate(${(360 / 60) * minute})`);
+  secondsElement.setAttribute("transform", `rotate(${(360 / 60) * second})`);
+
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
+
+textElement.addEventListener("click", () => {
+  showDate = !showDate;
+});
+
+document.querySelector("svg").style.display = "block";
